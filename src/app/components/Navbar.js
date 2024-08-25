1e9a41b29201fa091ccc370e7076452b671527e6
Umoh Udonwa <udonwajnr10@gmail.com>
2024-08-25 03:38:50 -0700

    commit

--- src/app/components/Navbar.js
+++ src/app/components/Navbar.js
@@ -15,7 +15,7 @@ const Navbar = () => (
       alignItems="flex-end"
     >
       <Link href="/" style={{ textDecoration: 'none', color: '#3A1212', borderBottom: '3px solid #FF2625' }}>Home</Link>
-      <a href="#exercises" style={{ textDecoration: 'none', color: '#3A1212' }}>Exercises</a>
+      <Link href="/exerciseplan" style={{ textDecoration: 'none', color: '#3A1212' }}>Exercises Plan</Link>
     </Stack>
   </Stack>
 );
